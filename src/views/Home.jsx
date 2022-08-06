1c1a34ca2c1a7083b7ad90f8af5b7ad4934e8cec
YouvaIBRAHIM <youva.ibrahim@gmail.com>
2022-08-06 22:55:16 +0200

    top the top of home page added

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,15 +1,18 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import PokemonsList from '../components/PokemonsList';
 import SearchBar from "../components/SearchBar";
 import { getPokemons, getAllPokemons } from '../services/Pokemons.service.js';
 import { useSelector, useDispatch } from 'react-redux';
 import { addPokemons, addAllPokemons } from '../reducers/PokemonsReducer';
-
+import styles from "../styles/Home.module.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowUp } from '@fortawesome/free-solid-svg-icons'
 
 function Home() {
   const dispatch = useDispatch();
   let { pokemons, allPokemons, nextUrl } = useSelector((state) => state.pokemons);
   const enableNextResult  = useRef(true);
+  const topPageButton  = useRef();
 
   const next = useRef();
 
@@ -46,12 +49,21 @@ function Home() {
         next.current = res.nextUrl;
       })
     }
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      topPageButton.current.style.display = "block";
+    } else {
+      topPageButton.current.style.display = "none";
+    }
   }
-
+  function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
   return (
     <div>
       <SearchBar enableNextResult={enableNextResult} />
       {pokemons && <PokemonsList pokemons={pokemons} />}
+      <button ref={topPageButton} onClick={scrollToTop} className={styles.topPageButton} title="Go to top"><FontAwesomeIcon icon={faArrowUp} /></button>
     </div>);
 }
 
